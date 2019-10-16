173c0e6a72101544ebc42d93bb83e4799c92dc53
Cavallaro Catello Stefano <stefano@innova-s.com>
2019-10-16 20:32:12 +0200

    Fix for stage

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,6 +1,6 @@
 //export const API_URL = 'https://api-gest.eneasys.net';
-//export const API_URL = 'https://api-gest.stage.eneasys.net';
-export const API_URL = 'http://localhost:8181';
+export const API_URL = 'https://api-gest.stage.eneasys.net';
+//export const API_URL = 'http://localhost:8181';
 
 
 export const SERVER_API_URL = API_URL + '/';
