e83f2243e02eaa45654424e89d02d89e25e1056a
Cavallaro Catello Stefano <stefano@innova-s.com>
2019-12-01 18:45:39 +0100

    Moved to application module all of aig and restored endpoint of stage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
@@ -19,15 +19,6 @@ import { LayoutModule } from 'app/layout/layout.module';
 
 import { ApiGestConsoleModule } from './main/api-gest-console/api-gest-console.module';
 import { SampleModule } from 'app/main/sample/sample.module';
-import { BASE_PATH } from 'api-gest';
-import { API_URL } from './app.constants';
-import { AigApolloDocumentModule } from './main/api-gest-console/modules/apollo-document/apollo-document.module';
-import { AigEopooModule } from './main/api-gest-console/modules/eopoo/eopoo.module';
-import { AigIamModule } from './main/api-gest-console/modules/iam/iam.module';
-import { AigManagementModule } from './main/api-gest-console/modules/management/management.module';
-
-import { AigCommonModule } from 'aig-common/common.module';
-import { AuthInterceptor } from 'aig-common/auth.interceptor';
 
 const appRoutes: Routes = [
     {
@@ -62,34 +53,12 @@ const appRoutes: Routes = [
         FuseSidebarModule,
         LayoutModule,
 
-        // Serve per interceptor
-        AigCommonModule,
-
-        // Serve per i route
+        // Application
         ApiGestConsoleModule,
-
-        // App modules
-        AigApolloDocumentModule,
-        AigEopooModule,
-
-        AigIamModule,
-        AigManagementModule,
-
-
 
         SampleModule,
     ],
-    providers: [
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AuthInterceptor,
-            multi: true
-        },
-        { 
-            provide: BASE_PATH,
-            useValue: API_URL
-        },
-    ],
+    providers: [ ],
     bootstrap: [
         AppComponent
     ]
