904370f12de466eeca46785e148764c4f05ce702
bluesky711711 <bluesky711711@gmail.com>
2020-01-30 03:35:37 +0800

    Create City Table Component

---
+++ src/aig-common/modules/standard/city-table/city-table.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'aig-city-table',
+    templateUrl: './city-table.component.html',
+    styleUrls: ['./city-table.component.scss']
+})
+export class AigCityTableComponent implements OnInit {
+    @Input() displayedColumns: string[];
+    @Input() dataSource: any[];
+    
+    constructor(
+        private router: Router,
+    ) { }
+
+    ngOnInit(): void { }
+
+    public detailCity(idCity: string){
+        this.router.navigate(['m8t', 'city', 'detail', idCity]);
+    }
+}
