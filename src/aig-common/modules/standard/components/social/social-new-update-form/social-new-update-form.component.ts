d20b96c213617145f93e2822f877525840f2b4e1
bluesky711711 <bluesky711711@gmail.com>
2020-02-06 21:28:03 +0800

    Modify Social components

--- src/aig-common/modules/standard/components/social/social-new-update-form/social-new-update-form.component.ts
+++ src/aig-common/modules/standard/components/social/social-new-update-form/social-new-update-form.component.ts
@@ -72,6 +72,10 @@ export class AigSocialNewUpdateFormComponent implements OnInit {
         this._fuseProgressBarService.hide();
     }
 
+    newSocial() {
+        this.setStep("form");
+    }
+
     private setStep(step: string){
         this.step.form = false;
         this.step.loading = false;
