866f74689ffeb9fadded43ab3678f740d2d6221a
Cavallaro Catello Stefano <stefano@innova-s.com>
2020-04-14 09:54:10 +0200

    Nuovi stati per riesamina

--- src/app/main/api-gest-console/modules/solidarity/components/solidarity-dashboard-page/solidarity-dashboard-page.component.ts
+++ src/app/main/api-gest-console/modules/solidarity/components/solidarity-dashboard-page/solidarity-dashboard-page.component.ts
@@ -16,13 +16,13 @@ export class AigSolidarityDashboardComponent extends GenericComponent {
         aigGenericComponentService: AigGenericComponentService,
     ) { super(aigGenericComponentService) }
 
-    displayedColumns = [ "surname", "name", "cf", "amount", "familyTotal" ]
-    displayedColumnsB = [ "surname", "name", "cf", "amount", "incomeMar", "familyTotal" ]
+    displayedColumns = ["surname", "name", "cf", "amount", "familyTotal"]
+    displayedColumnsB = ["surname", "name", "cf", "amount", "incomeMar", "familyTotal"]
 
     foodProductRequestDTOs: FoodProductRequestDTO[] = [];
 
     totalAmountApproved: number = 0;
-    
+
     totalAmountA: number = 0;
     foodProductRequestDTOsA: FoodProductRequestDTO[] = [];
     totalAmountB: number = 0;
@@ -41,49 +41,68 @@ export class AigSolidarityDashboardComponent extends GenericComponent {
     totalAmountReleased: number = 0;
     totalReleased: number = 0;
 
+    omonimi = {};
+
     async loadComponent() {
-        this.foodProductRequestDTOs = await this.foodProductRequestResourceService.getAllFoodProductRequestsUsingGET(null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,1000,null,null,null,null,null).toPromise();
+        this.foodProductRequestDTOs = await this.foodProductRequestResourceService.getAllFoodProductRequestsUsingGET(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 1000, null, null, null, null, null).toPromise();
 
         let foodProductRequestDTOsA: FoodProductRequestDTO[] = [];
         let foodProductRequestDTOsB: FoodProductRequestDTO[] = [];
         let foodProductRequestDTOsC: FoodProductRequestDTO[] = [];
 
         this.foodProductRequestDTOs.forEach(foodProductRequestDTO => {
-            if(foodProductRequestDTO.note != "97") {
+            if (foodProductRequestDTO.note != "97") {
                 this.totalUnique = this.totalUnique + 1;
             }
 
-            if(foodProductRequestDTO.note == "95") {
+            if (foodProductRequestDTO.note == "95") {
                 this.totalAmountReleased = this.totalAmountReleased + this.aigSolidarityRequestCalculatorService.calculate(foodProductRequestDTO);
                 this.totalReleased = this.totalReleased + 1;
             }
 
-            if(foodProductRequestDTO.note == "96") {
+            if (foodProductRequestDTO.note == "96") {
                 this.totalAmountInamissible = this.totalAmountInamissible + this.aigSolidarityRequestCalculatorService.calculate(foodProductRequestDTO);
                 this.totalInamissible = this.totalInamissible + 1;
             }
 
-            if(foodProductRequestDTO.note == "99") {
+            if (foodProductRequestDTO.note == "99") {
                 this.totalAmountNotAproved = this.totalAmountNotAproved + this.aigSolidarityRequestCalculatorService.calculate(foodProductRequestDTO);
                 this.totalNotAproved = this.totalNotAproved + 1;
             }
 
-            if(foodProductRequestDTO.note == "2") {
+            if (foodProductRequestDTO.note == "2") {
                 this.totalAmountApproved = this.totalAmountApproved + this.aigSolidarityRequestCalculatorService.calculate(foodProductRequestDTO);
 
-                if(foodProductRequestDTO.requestStatusA) {
+                if (foodProductRequestDTO.requestStatusA) {
                     this.totalAmountA = this.totalAmountA + this.aigSolidarityRequestCalculatorService.calculate(foodProductRequestDTO);
                     foodProductRequestDTOsA.push(foodProductRequestDTO);
                 }
-                if(foodProductRequestDTO.requestStatusB) {
+                if (foodProductRequestDTO.requestStatusB) {
                     this.totalAmountB = this.totalAmountB + this.aigSolidarityRequestCalculatorService.calculate(foodProductRequestDTO);
                     foodProductRequestDTOsB.push(foodProductRequestDTO);
                 }
-                if(foodProductRequestDTO.requestStatusC) {
+                if (foodProductRequestDTO.requestStatusC) {
                     this.totalAmountC = this.totalAmountC + this.aigSolidarityRequestCalculatorService.calculate(foodProductRequestDTO);
                     foodProductRequestDTOsC.push(foodProductRequestDTO);
                 }
             }
+
+            // Casi di omonimia
+            /*
+            if (foodProductRequestDTO.note != "97") {
+                this.foodProductRequestDTOs.forEach(foodProductRequestDTO2 => {
+                    if (foodProductRequestDTO2.note != "97") {
+                        if (foodProductRequestDTO.familyUnit.taxId.replace(" ", "").toUpperCase() != foodProductRequestDTO2.familyUnit.taxId.replace(" ", "").toUpperCase()) {
+                            if (foodProductRequestDTO.familyUnit.name.replace(" ", "").toUpperCase() == foodProductRequestDTO2.familyUnit.name.replace(" ", "").toUpperCase()) {
+                                if (foodProductRequestDTO.familyUnit.surname.replace(" ", "").toUpperCase() == foodProductRequestDTO2.familyUnit.surname.replace(" ", "").toUpperCase()) {
+                                    this.omonimi[foodProductRequestDTO.familyUnit.surname.replace(" ", "").toUpperCase() + "-" + foodProductRequestDTO.familyUnit.name.replace(" ", "").toUpperCase()] = true;
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+            */
         });
         this.foodProductRequestDTOsA = foodProductRequestDTOsA;
         this.foodProductRequestDTOsB = foodProductRequestDTOsB;
