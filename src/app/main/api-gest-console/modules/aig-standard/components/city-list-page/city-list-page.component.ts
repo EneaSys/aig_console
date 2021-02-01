89545e4677039ec4e717a9f4d37d6dedb96a6ff5
Alberto Alfieri <alberto.alfieri@live.it>
2021-02-01 17:26:08 +0100

    added filters and pagination in aig standard / city

--- src/app/main/api-gest-console/modules/aig-standard/components/city-list-page/city-list-page.component.ts
+++ src/app/main/api-gest-console/modules/aig-standard/components/city-list-page/city-list-page.component.ts
@@ -4,6 +4,8 @@ import { AigGenericComponentService } from 'app/main/api-gest-console/generic-co
 import { CityResourceService, CityDTO } from 'aig-standard';
 import { MatDialog } from '@angular/material/dialog';
 import { AigCityNewUpdateModalComponent } from '../city-new-update-modal/city-new-update-modal.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar, PageEvent } from '@angular/material';
 
 @Component({
     templateUrl: './city-list-page.component.html',
@@ -12,18 +14,106 @@ import { AigCityNewUpdateModalComponent } from '../city-new-update-modal/city-ne
 export class AigCityListPageComponent extends GenericComponent {
     constructor(
         private cityResourceService: CityResourceService,
+        private _formBuilder: FormBuilder,
         private dialog: MatDialog,
+		private _snackBar: MatSnackBar,
         aigGenericComponentService: AigGenericComponentService,
     ) { super(aigGenericComponentService) }
 
-    displayedColumns: string[] = ['id', 'code', 'name','wikiCode', 'buttons'];
-    citys: CityDTO[];
+    loadPage() {
+		this.initCitySearch();
 
-    async loadComponent() {
-        this.citys = await this.cityResourceService.getAllCitiesUsingGET().toPromise();
+		this.showAllCity();
+	}
+
+	reloadPage() {
+		this.showAllCity();
+	}
+
+    //			---- CITY TABLE AND SEARCH SECTION ----
+    
+    citySearchFormGroup: FormGroup;
+	cityPagination: any;
+	cityFilters: any;
+
+	cityLength: number;
+	cityDTOs: CityDTO[];
+	cityError: any;
+
+    cityDC: string[];
+    
+    private initCitySearch() {
+		this.cityPagination = {
+			size: 10,
+			page: 0,
+		}
+
+		this.citySearchFormGroup = this._formBuilder.group({
+			id: [''],
+			name: [''],
+		});
+
+		this.cityDC = ['id', 'code', 'name','wikiCode', 'buttons'];
     }
+    
+    private clearFiltersCity() {
+		this.cityFilters = {
+			id: null,
+			name: null,
+		}
+    }
+    
+    private async searchCity(page: number) {
+		this.cityPagination.page = page;
+		this.cityDTOs = null;
+		try {
+			this.cityLength = await this.cityResourceService.countCitiesUsingGET(null,null,null,null,null,null,this.cityFilters.id,null,null,null,null,null,null,null,this.cityFilters.name,null,null,null,null,null,null,null,null,null,null,null,null,).toPromise();
+
+			if(this.cityLength == 0) {
+				this._snackBar.open("Nessun valore trovato con questi parametri!", null, {duration: 2000,});
+				this.cityDTOs = [];
+				return;
+			}
+
+			this.cityDTOs = await this.cityResourceService.getAllCitiesUsingGET(null,null,null,null,null,null,this.cityFilters.id,null,null,null,null,null,null,null,this.cityFilters.name,null,null,null,null,null,this.cityPagination.page,this.cityPagination.size,null,null,null,null,null,null,null,null,).toPromise();
+		} catch (e) {
+			this.cityError = e;
+		}
+    }
+    
+    showAllCity() {
+		this.clearFiltersCity();
+		this.searchCity(0);
+    }
+    
+    resetFiltersCity() {
+		this.citySearchFormGroup.reset();
+		this.showAllCity();
+    }
+    
+    cityPaginationEvent(pageEvent: PageEvent) {
+		this.cityPagination.size = pageEvent.pageSize;
+		this.searchCity(pageEvent.pageIndex);
+	}
+
+    citySearchWithFilter() {
+		let searchedId = this.citySearchFormGroup.controls.id.value;
+
+		if(searchedId != null) {
+			this.clearFiltersCity();
+			this.citySearchFormGroup.reset();
+			this.cityFilters.id = searchedId;
+			this.searchCity(0);
+			return;
+		}
+
+		this.cityFilters.name = this.citySearchFormGroup.controls.name.value;
+
+		this.searchCity(0);
+	}
 
     newCity(){
         this.dialog.open(AigCityNewUpdateModalComponent, { data: { city: {} } });
     }
+    //			---- !CITY TABLE AND SEARCH SECTION ----
 }
