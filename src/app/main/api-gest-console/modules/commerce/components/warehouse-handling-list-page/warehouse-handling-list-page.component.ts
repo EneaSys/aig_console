d233995f186eb7df41e05c60d479fab4f8206227
Alberto Alfieri <alberto.alfieri@live.it>
2021-02-05 18:36:16 +0100

    start creation of warehouse handling list page

---
+++ src/app/main/api-gest-console/modules/commerce/components/warehouse-handling-list-page/warehouse-handling-list-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'warehouse-handling-list-page',
+    templateUrl: './warehouse-handling-list-page.component.html',
+    styleUrls: ['./warehouse-handling-list-page.component.scss']
+})
+export class AigWarehouseHandlingListPageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
