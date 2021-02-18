e9053d29befc81e7624d5c0bdd0b3388f606bbaa
Catello Stefano Cavallaro <stefano@stepo.biz>
2021-02-18 16:22:59 +0100

    Correction of work

--- src/app/main/api-gest-console/modules/commerce/components/inventory-item-combination-list-page/inventory-item-combination-list-page.component.ts
+++ src/app/main/api-gest-console/modules/commerce/components/inventory-item-combination-list-page/inventory-item-combination-list-page.component.ts
@@ -49,7 +49,7 @@ export class AigInventoryItemCombinationListPageComponent extends GenericCompone
 
 	
 	private initInventoryItemCombinationSearch() {
-		this.inventoryItemCombinationDC = ["id", "name", "buttons"];
+		this.inventoryItemCombinationDC = ["id", "name", "inventoryItemName"];
 
 		this.inventoryItemCombinationPaginationSize = 10;
 		
