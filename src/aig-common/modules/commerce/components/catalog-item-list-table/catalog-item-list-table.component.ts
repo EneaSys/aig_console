c2d0c3c6a8a3f62c3a0b696f597fafc372332919
Naomi Celentano <naocel20@gmail.com>
2021-03-24 22:43:54 +0100

    add buttons in catalog-item

--- src/aig-common/modules/commerce/components/catalog-item-list-table/catalog-item-list-table.component.ts
+++ src/aig-common/modules/commerce/components/catalog-item-list-table/catalog-item-list-table.component.ts
@@ -1,4 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { FuseProgressBarService } from '@fuse/components/progress-bar/progress-bar.service';
+import { CatalogItemDTO, CatalogItemResourceService } from 'aig-commerce';
+import { EventService } from 'aig-common/event-manager/event.service';
+import { AigCatalogItemNewUpdateDialogComponent } from 'app/main/api-gest-console/modules/commerce/components/catalog-item-new-update-dialog/catalog-item-new-update-dialog.component';
+import { AigCatalogItemNewUpdateFormComponent } from '../catalog-item-new-update-form/catalog-item-new-update-form.component';
 
 @Component({
     selector: 'aig-catalog-item-list-table',
@@ -13,7 +19,30 @@ export class AigCatalogItemListTableComponent implements OnInit {
 	@Input()
     error: any;
     
-    constructor() { }
+    constructor(
+        private catalogItemResourceService: CatalogItemResourceService,
+        private eventService: EventService,
+        private _fuseProgressBarService: FuseProgressBarService,
+        private _snackBar: MatSnackBar,
+        private dialog: MatDialog,
+    ) { }
 
     ngOnInit(): void { }
+    async deleteCatalogItem(id: number) {
+        this._fuseProgressBarService.show();
+
+        try {
+            await this.catalogItemResourceService.deleteCatalogItemUsingDELETE(id).toPromise();
+            this._snackBar.open(`Catalog Item: '${id}' deleted.`, null, { duration: 2000, });
+
+            this.eventService.reloadCurrentPage();
+        } catch (e) {
+            this._snackBar.open(`Error during deleting catalog item: '${id}'. (${e.message})`, null, { duration: 5000, });
+        }
+        this._fuseProgressBarService.hide();
+    }
+
+    editCatalogItem(catalogItemDTO: CatalogItemDTO) {
+        this.dialog.open(AigCatalogItemNewUpdateDialogComponent, { data: { catalogItem: catalogItemDTO } });
+    }
 }
