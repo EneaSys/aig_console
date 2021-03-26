c072533098978c87991a13bc5c00becea471ca7b
Giacomo Trinchillo <trinchillogiacomomattia@gmail.com>
2021-03-26 23:18:28 +0100

    delete button on warehouse detail page

--- src/app/main/api-gest-console/modules/commerce/components/warehouse-detail-page/warehouse-detail-page.component.ts
+++ src/app/main/api-gest-console/modules/commerce/components/warehouse-detail-page/warehouse-detail-page.component.ts
@@ -1,42 +1,61 @@
 import { Component } from "@angular/core";
-import { MatDialog } from "@angular/material";
-import { ActivatedRoute } from "@angular/router";
+import { MatDialog, MatSnackBar } from "@angular/material";
+import { ActivatedRoute, Router } from "@angular/router";
+import { FuseProgressBarService } from "@fuse/components/progress-bar/progress-bar.service";
 import { WarehouseDTO, WarehouseResourceService } from "aig-commerce";
 import { GenericComponent } from "app/main/api-gest-console/generic-component/generic-component";
 import { AigGenericComponentService } from "app/main/api-gest-console/generic-component/generic-component.service";
 import { AigWarehouseNewUpdateModalComponent } from "../warehouse-new-update-modal/warehouse-new-update-modal.component";
 
 @Component({
-	selector: 'aig-warehouse-detail-page',
-	templateUrl: './warehouse-detail-page.component.html',
-	styleUrls: ['./warehouse-detail-page.component.scss']
+  selector: 'aig-warehouse-detail-page',
+  templateUrl: './warehouse-detail-page.component.html',
+  styleUrls: ['./warehouse-detail-page.component.scss']
 })
 export class AigWarehouseDetailPageComponent extends GenericComponent {
-    constructor(
-        private warehouseResourceService: WarehouseResourceService,
-        private route: ActivatedRoute,
-        private dialog: MatDialog,
-        aigGenericComponentService: AigGenericComponentService,
-        ) { super(aigGenericComponentService) }
+  constructor(
+    private warehouseResourceService: WarehouseResourceService,
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+    private _fuseProgressBarService: FuseProgressBarService,
+    private router: Router,
+    private _snackBar: MatSnackBar,
+    aigGenericComponentService: AigGenericComponentService,
+  ) { super(aigGenericComponentService) }
 
-	warehouseDTO: WarehouseDTO;
+  warehouseDTO: WarehouseDTO;
 
-	warehouseConfig = {
-        details: true,
-        removeWarehouse: null,
+  warehouseConfig = {
+    details: true,
+    removeWarehouse: null,
+  }
+
+  loadPage() {
+    this.warehouseDTO = this.route.snapshot.data.warehouse;
+
+    this.warehouseConfig.removeWarehouse = this.warehouseDTO;
+  }
+
+  async reloadPage() {
+    this.warehouseDTO = await this.warehouseResourceService.getWarehouseUsingGET(this.warehouseDTO.id).toPromise();
+  }
+
+  editWarehouse(warehouseDTO: WarehouseDTO) {
+    this.dialog.open(AigWarehouseNewUpdateModalComponent, { data: { warehouse: warehouseDTO } });
+  }
+
+  async deleteWarehouse(id: number) {
+    this._fuseProgressBarService.show();
+
+    try {
+      await this.warehouseResourceService.deleteWarehouseUsingDELETE(id).toPromise();
+
+      this._snackBar.open(`Warehouse: '${id}' deleted.`, null, { duration: 2000, });
+
+      this.router.navigate(['/commerce', 'warehouse']);
+    } catch (e) {
+      this._snackBar.open(`Error during deleting warehouse: '${id}'. (${e.message})`, null, { duration: 5000, });
     }
-
-    loadPage() {
-		this.warehouseDTO = this.route.snapshot.data.warehouse;
-
-		this.warehouseConfig.removeWarehouse = this.warehouseDTO;
-	}
-
-	async reloadPage() {
-		this.warehouseDTO = await this.warehouseResourceService.getWarehouseUsingGET(this.warehouseDTO.id).toPromise();
-	}
-	
-    editWarehouse(warehouseDTO: WarehouseDTO) {
-		this.dialog.open(AigWarehouseNewUpdateModalComponent, { data: { warehouse: warehouseDTO } });
-    }
+    this._fuseProgressBarService.hide();
+  }
 }
