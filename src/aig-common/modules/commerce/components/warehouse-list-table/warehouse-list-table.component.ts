e97c2cda5bb84001dff9c3cbddfa19ed6cbae4f1
Giacomo Trinchillo <trinchillogiacomomattia@gmail.com>
2021-03-26 23:13:32 +0100

    edit adn delete button on Warehouse e Seller page

--- src/aig-common/modules/commerce/components/warehouse-list-table/warehouse-list-table.component.ts
+++ src/aig-common/modules/commerce/components/warehouse-list-table/warehouse-list-table.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material';
+import { MatDialog, MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
 import { FuseProgressBarService } from '@fuse/components/progress-bar/progress-bar.service';
-import { WarehouseResourceService } from 'aig-commerce';
+import { WarehouseDTO, WarehouseResourceService } from 'aig-commerce';
 import { EventService } from 'aig-common/event-manager/event.service';
+import { AigWarehouseNewUpdateModalComponent } from 'app/main/api-gest-console/modules/commerce/components/warehouse-new-update-modal/warehouse-new-update-modal.component';
 
 @Component({
     selector: 'aig-warehouse-list-table',
@@ -24,7 +25,26 @@ export class AigWarehouseListTableComponent implements OnInit {
         private _snackBar: MatSnackBar,
         private _fuseProgressBarService: FuseProgressBarService,
         private router: Router,
+        private dialog: MatDialog,
     ) { }
 
     ngOnInit(): void { }
+
+    async deleteWarehouse(id: number) {
+        this._fuseProgressBarService.show();
+
+        try {
+            await this.warehouseResourceService.deleteWarehouseUsingDELETE(id).toPromise();
+            this._snackBar.open(`Warehouse: '${id}' deleted.`, null, { duration: 2000, });
+
+            this.eventService.reloadCurrentPage();
+        } catch (e) {
+            this._snackBar.open(`Error during deleting warehouse: '${id}'. (${e.message})`, null, { duration: 5000, });
+        }
+        this._fuseProgressBarService.hide();
+    }
+
+    editWarehouseHandling(warehouseDTO: WarehouseDTO) {
+        this.dialog.open(AigWarehouseNewUpdateModalComponent, { data: { warehouse: warehouseDTO } });
+    }
 }
