1278130b64cf0d826f8d8e9e842fb1e01d6039d7
Alberto Alfieri <alberto.alfieri@live.it>
2021-04-01 12:55:47 +0200

    task AIG-512 completed

--- src/app/main/api-gest-console/modules/commerce/components/producer-detail-page/producer-detail-page.component.ts
+++ src/app/main/api-gest-console/modules/commerce/components/producer-detail-page/producer-detail-page.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog, MatSnackBar } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FuseProgressBarService } from '@fuse/components/progress-bar/progress-bar.service';
-import { ProducerDTO, ProducerResourceService } from 'aig-commerce';
+import { InventoryItemDTO, InventoryItemResourceService, ProducerDTO, ProducerResourceService } from 'aig-commerce';
 import { GenericComponent } from 'app/main/api-gest-console/generic-component/generic-component';
 import { AigGenericComponentService } from 'app/main/api-gest-console/generic-component/generic-component.service';
 import { AigProducerNewUpdateModalComponent } from '../producer-new-update-modal-component/producer-new-update-modal.component';
@@ -13,42 +13,64 @@ import { AigProducerNewUpdateModalComponent } from '../producer-new-update-modal
 	styleUrls: ['./producer-detail-page.component.scss']
 })
 export class AigProducerDetailPageComponent extends GenericComponent {
-    constructor(
+	constructor(
 		private _snackBar: MatSnackBar,
 		private router: Router,
 		private _fuseProgressBarService: FuseProgressBarService,
-        private producerResourceService: ProducerResourceService,
-        private route: ActivatedRoute,
-        private dialog: MatDialog,
-        aigGenericComponentService: AigGenericComponentService,
-        ) { super(aigGenericComponentService) }
+		private producerResourceService: ProducerResourceService,
+		private inventoryItemResourceService: InventoryItemResourceService,
+		private route: ActivatedRoute,
+		private dialog: MatDialog,
+		aigGenericComponentService: AigGenericComponentService,
+	) { super(aigGenericComponentService) }
 
 	producerDTO: ProducerDTO;
 
-    loadPage() {
+	loadPage() {
 		this.producerDTO = this.route.snapshot.data.producer;
+		this.loadOther();
 	}
 
 	async reloadPage() {
 		this.producerDTO = await this.producerResourceService.getProducerUsingGET(this.producerDTO.id).toPromise();
+		this.loadOther();
 	}
-	
+
+	async loadOther() {
+		this.loadInventoryItem();
+	}
+
+	editProducer(producerDTO: ProducerDTO) {
+		this.dialog.open(AigProducerNewUpdateModalComponent, { data: { producer: producerDTO } });
+	}
+
 	async deleteProducer(id: number) {
 		this._fuseProgressBarService.show();
-	
+
 		try {
 			await this.producerResourceService.deleteProducerUsingDELETE(id).toPromise();
-	
+
 			this._snackBar.open(`Producer: '${id}' deleted.`, null, { duration: 2000, });
-			
+
 			this.router.navigate(['/commerce', 'producer']);
 		} catch (e) {
 			this._snackBar.open(`Error during deleting producer: '${id}'. (${e.message})`, null, { duration: 5000, });
 		}
 		this._fuseProgressBarService.hide();
-	  }
+	}
 
-    editProducer(producerDTO: ProducerDTO) {
-		this.dialog.open(AigProducerNewUpdateModalComponent, { data: { producer: producerDTO } });
-    }
+	inventoryItemDC: string[] = ["id", "inventoryCategoryName", "name", "buttons",];
+	inventoryItemDTOs: InventoryItemDTO[];
+	inventoryItemError: any;
+	async loadInventoryItem() {
+		let filters = {
+			producerIdEquals: this.producerDTO.id
+		};
+		try {
+			this.inventoryItemDTOs = await this.inventoryItemResourceService.getAllInventoryItemsUsingGET(filters).toPromise();
+		} catch (e) {
+			this.inventoryItemError = e;
+		}
+	}
+
 }
