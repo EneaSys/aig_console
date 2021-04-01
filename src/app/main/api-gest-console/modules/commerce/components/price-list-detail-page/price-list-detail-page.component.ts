f9c3235de80195420846af458fa2028990086299
Alberto Alfieri <alberto.alfieri@live.it>
2021-04-01 16:09:18 +0200

    task AIG-521 completed

--- src/app/main/api-gest-console/modules/commerce/components/price-list-detail-page/price-list-detail-page.component.ts
+++ src/app/main/api-gest-console/modules/commerce/components/price-list-detail-page/price-list-detail-page.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog, MatSnackBar } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FuseProgressBarService } from '@fuse/components/progress-bar/progress-bar.service';
-import { PriceListDTO, PriceListResourceService } from 'aig-commerce';
+import { PriceListDTO, PriceListItemDTO, PriceListItemResourceService, PriceListResourceService } from 'aig-commerce';
 import { GenericComponent } from 'app/main/api-gest-console/generic-component/generic-component';
 import { AigGenericComponentService } from 'app/main/api-gest-console/generic-component/generic-component.service';
 import { AigPriceListNewUpdateDialogComponent } from '../price-list-new-update-dialog/price-list-new-update-dialog.component';
@@ -18,6 +18,7 @@ export class AigPriceListDetailPageComponent extends GenericComponent {
         private _fuseProgressBarService: FuseProgressBarService,
         private _snackBar: MatSnackBar,
         private priceListResourceService: PriceListResourceService,
+        private priceListItemResourceService: PriceListItemResourceService,
         private route: ActivatedRoute,
         private dialog: MatDialog,
         aigGenericComponentService: AigGenericComponentService,
@@ -27,11 +28,21 @@ export class AigPriceListDetailPageComponent extends GenericComponent {
 
     loadPage() {
         this.priceListDTO = this.route.snapshot.data.priceList;
+        this.loadOther();
     }
 
     async reloadPage() {
 		this.priceListDTO = await this.priceListResourceService.getPriceListUsingGET(this.priceListDTO.id).toPromise();
+        this.loadOther();
 	}
+
+    async loadOther() {
+        this.loadPriceListItem();
+    }
+
+    editPriceList(priceListDTO: PriceListDTO) {
+        this.dialog.open(AigPriceListNewUpdateDialogComponent, { data: { priceList: priceListDTO } });
+    }
 
     async deletePriceList(id: number) {
         this._fuseProgressBarService.show();
@@ -48,7 +59,17 @@ export class AigPriceListDetailPageComponent extends GenericComponent {
         this._fuseProgressBarService.hide();
       }
 
-    editPriceList(priceListDTO: PriceListDTO) {
-        this.dialog.open(AigPriceListNewUpdateDialogComponent, { data: { priceList: priceListDTO } });
-    }
+      priceListItemDC: string[] = ["id", "catalog", "catalogItem", "amount", "buttons"];
+      priceListItemDTOs: PriceListItemDTO[];
+      priceListItemError: any;
+      async loadPriceListItem() {
+          let filters = {
+            priceListIdEquals: this.priceListDTO.id
+          };
+          try {
+              this.priceListItemDTOs = await this.priceListItemResourceService.getAllPriceListItemsUsingGET(filters).toPromise();
+          } catch (e) {
+              this.priceListItemError = e;
+          }
+      }
 }
