63d80f613f91a1be6df67b620e7209479e4d2868
Giacomo Trinchillo <trinchillogiacomomattia@gmail.com>
2021-04-22 13:37:07 +0200

    contact and referent in eopo detsil page

--- src/app/main/api-gest-console/modules/aig-generic/components/eopoo-detail-page/eopoo-detail-page.component.ts
+++ src/app/main/api-gest-console/modules/aig-generic/components/eopoo-detail-page/eopoo-detail-page.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 
-import { EopooResourceService, EopooDTO, AddressResourceService, AddressDTO } from 'aig-generic';
+import { EopooResourceService, EopooDTO, AddressResourceService, AddressDTO, ContactResourceService, ContactDTO, ReferentDTO, ReferentResourceService } from 'aig-generic';
 
 import { GenericComponent } from 'app/main/api-gest-console/generic-component/generic-component';
 import { AigGenericComponentService } from 'app/main/api-gest-console/generic-component/generic-component.service';
@@ -12,6 +12,8 @@ import { ContextUserEopooResourceService, ContextUserEopooDTO } from 'api-gest';
 import { SellerResourceService, SellerDTO } from 'aig-commerce';
 import { FuseProgressBarService } from '@fuse/components/progress-bar/progress-bar.service';
 import { MatSnackBar } from '@angular/material';
+import { AigReferentNewUpdateDialogComponent } from '../referent-new-update-dialog/referent-new-update-dialog.component';
+import { AigContactNewUpdateDialogComponent } from '../contact-new-update-dialog/contact-new-update-dialog.component';
 
 
 @Component({
@@ -26,6 +28,8 @@ export class AigEopooDetailPageComponent extends GenericComponent {
         private router: Router,
         private route: ActivatedRoute,
         private eopooResourceService: EopooResourceService,
+        private contactResourceService: ContactResourceService,
+        private referentResourceService: ReferentResourceService,
         private contextUserEopooResourceService: ContextUserEopooResourceService,
         private sellerResourceService: SellerResourceService,
         private addressResourceService: AddressResourceService,
@@ -37,11 +41,15 @@ export class AigEopooDetailPageComponent extends GenericComponent {
     loadPage() {
         this.eopooDTO = this.route.snapshot.data.eopoo;
         this.loadAddress();
+        this.loadContact();
+        this.loadReferent();
     }
 
     async reloadPage() {
         this.eopooDTO = await this.eopooResourceService.getEopooUsingGET(this.eopooDTO.id).toPromise();
         this.loadAddress();
+        this.loadContact();
+        this.loadReferent();
     }
 
 
@@ -54,32 +62,63 @@ export class AigEopooDetailPageComponent extends GenericComponent {
         let filters = {
             eopooIdEquals: this.eopooDTO.id,
         };
-        this.addressDTOs = await this.addressResourceService.getAllAddressesUsingGET(filters).toPromise();  
+        this.addressDTOs = await this.addressResourceService.getAllAddressesUsingGET(filters).toPromise();
+    }
+
+    addAddress(eopooDTO: EopooDTO) {
+        this.dialog.open(AigAddressNewUpdateModalComponent, { data: { eopoo: eopooDTO } });
     }
 
 
+    contactDC: string[] = ["contactTypeCode", "eopooId", 'buttons'];
+    contactDTOs: ContactDTO[];
+    contactError: any;
 
-    
+    async loadContact() {
+        let filters = {
+            eopooIdEquals: this.eopooDTO.id,
+        };
+        this.contactDTOs = await this.contactResourceService.getAllContactsUsingGET(filters).toPromise();
+    }
+
+    addContact(eopooDTO: EopooDTO) {
+        this.dialog.open(AigContactNewUpdateDialogComponent, { data: { eopoo: eopooDTO } });
+    }
+
+
+    referentDC: string[] = ["firstname", "lastname", "position", "buttons"];
+    referentDTOs: ReferentDTO[];
+    referentError: any;
+
+    async loadReferent() {
+        let filters = {
+            eopooIdEquals: this.eopooDTO.id,
+        };
+        this.referentDTOs = await this.referentResourceService.getAllReferentsUsingGET(filters).toPromise();
+    }
+
+    addReferent(eopooDTO: EopooDTO) {
+        this.dialog.open(AigReferentNewUpdateDialogComponent, { data: { eopoo: eopooDTO } });
+    }
+
+
     editEopoo(eopooDTO: EopooDTO) {
         this.dialog.open(AigEopooNewModalComponent, { data: { eopoo: eopooDTO } });
     }
 
     async deleteEopoo(id: number) {
         this._fuseProgressBarService.show();
-    
+
         try {
             await this.eopooResourceService.deleteEopooUsingDELETE(id).toPromise();
-    
+
             this._snackBar.open(`Eopoo: '${id}' deleted.`, null, { duration: 2000, });
-            
+
             this.router.navigate(['/g5c', 'eopoo']);
         } catch (e) {
             this._snackBar.open(`Error during deleting eopoo: '${id}'. (${e.message})`, null, { duration: 5000, });
         }
         this._fuseProgressBarService.hide();
-      }
-
-    addAddress(eopooDTO: EopooDTO) {
-        this.dialog.open(AigAddressNewUpdateModalComponent, { data: { eopoo: eopooDTO } });
     }
+
 }
