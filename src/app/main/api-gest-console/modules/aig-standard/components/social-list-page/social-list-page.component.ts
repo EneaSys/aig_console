ed9bfef3446f3b440fff171ddaff3b0047330a2a
Giacomo Trinchillo <trinchillogiacomomattia@gmail.com>
2021-04-22 14:16:44 +0200

    social list table fix

--- src/app/main/api-gest-console/modules/aig-standard/components/social-list-page/social-list-page.component.ts
+++ src/app/main/api-gest-console/modules/aig-standard/components/social-list-page/social-list-page.component.ts
@@ -72,7 +72,7 @@ export class AigSocialListPageComponent extends GenericComponent {
 		this.socialFilters.size = this.socialPaginationSize;
 
 		try {
-			this.socialLength = await this.socialResourceService.countSocialsUsingGET().toPromise();
+			this.socialLength = await this.socialResourceService.countSocialsUsingGET(this.socialFilters).toPromise();
 
 			if(this.socialLength == 0) {
 				this._snackBar.open("Nessun valore trovato con questi parametri!", null, {duration: 2000,});
@@ -80,7 +80,7 @@ export class AigSocialListPageComponent extends GenericComponent {
 				return;
 			}
 
-			this.socialDTOs = await this.socialResourceService.getAllSocialsUsingGET().toPromise();
+			this.socialDTOs = await this.socialResourceService.getAllSocialsUsingGET(this.socialFilters).toPromise();
 		} catch (e) {
 			this.socialError = e;
 		}
