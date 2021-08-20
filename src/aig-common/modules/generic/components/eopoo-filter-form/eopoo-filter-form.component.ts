8cf9cb077d1a33e983cacd02ec19685183cc520a
Giacomo_Trinchillo <trinchillogiacomomattia@gmail.com>
2021-08-20 10:03:49 +0200

    add agal generic component

---
+++ src/aig-common/modules/generic/components/eopoo-filter-form/eopoo-filter-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'eopoo-filter-form',
+    templateUrl: './eopoo-filter-form.component.html',
+    styleUrls: ['./eopoo-filter-form.component.scss']
+})
+export class EopooFilterFormComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
