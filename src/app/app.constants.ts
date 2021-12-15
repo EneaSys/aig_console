bfce033b8cb81d915342be7346886df8634ee772
Cavallaro Catello Stefano <stefano@stepo.biz>
2021-12-15 23:05:05 +0100

    endpoint

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
-export const API_URL = 'https://api-gest.eneasys.net';
-//export const API_URL = 'https://api-gest.stage.eneasys.net';
+//export const API_URL = 'https://api-gest.eneasys.net';
+export const API_URL = 'https://api-gest.stage.eneasys.net';
 //export const API_URL = 'http://localhost:8182';
 
 export const SERVER_API_URL = API_URL + '/';
